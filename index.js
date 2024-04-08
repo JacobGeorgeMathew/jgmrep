006a9d48fdeecc1f5a9164f7c7d7a4c16081a635
Jacob George Mathew <jm1051204@gmail.com>
2024-04-08 19:21:22 +0530

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log(`Hello`);
+
+console.log(`This is my first Website!`);
+console.log(`I want to become a Web volunteer in IEDC!`);
+console.log(`but I am not an expert in Web Development!`);
+console.log(`I am a beginner!`);
+console.log(`but I am sure that I can learn Web development!`);
+console.log(`so please consider this small work as my application!`);
